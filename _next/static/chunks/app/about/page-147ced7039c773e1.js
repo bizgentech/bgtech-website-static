167ad70ef3b456b3dd0090b60c3799007e7a1830
User <user@Users-MacBook-Pro.local>
2026-05-26 20:05:21 -0400

    Wire contact form to Web3Forms, remove budget field

--- _next/static/chunks/app/about/page-147ced7039c773e1.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[301,805,115,444,549,289,739],{3695:function(n,e,u){Promise.resolve().then(u.t.bind(u,2972,23))}},function(n){n.O(0,[972,971,117,744],function(){return n(n.s=3695)}),_N_E=n.O()}]);
